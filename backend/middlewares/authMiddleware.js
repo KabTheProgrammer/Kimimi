438d7199341db65e579437c163d969332f43b04b
KabTheProgrammer <ikabiru038@gmai.com>
2024-11-01 09:56:01 +0000

    update authmiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -5,26 +5,30 @@ import asyncHandler from './asyncHandler.js'
 const authenticate = asyncHandler(async (req, res, next) => {
     let token;
 
-    // Read Jwt from the 'jwt' cookie
-
-    token = req.cookies.jwt
-
-    if(token) {
-        try {
-            
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            req.user = await User.findById(decoded.userId).select("-password")
-            next();
-
-        } catch (error) {
-            res.status(401)
-            throw new Error("Not authorized, token failed")
-        }
-    } else{
-        res.status(401)
-        throw new Error("Not authorized, no token")
+    // Check for token in Authorization header (Bearer token)
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
     }
-})
+    // Or check for token in cookies
+    else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    } else {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+});
+
 
 //Check if user is admin
 
