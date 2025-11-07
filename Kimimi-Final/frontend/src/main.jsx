703dc0f3af17751ef30ced1ff6fda91d03b84e4f
KabTheProgrammer <ikabiru038@email.com>
2025-11-07 23:24:46 +0000

    fix import

--- Kimimi-Final/frontend/src/main.jsx
+++ Kimimi-Final/frontend/src/main.jsx
@@ -1,65 +1,55 @@
-import React from 'react'
-import ReactDOM from "react-dom/client"
-import App from './App.jsx'
-import './index.css'
-import { 
-  Route, 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  Route,
   RouterProvider,
   createRoutesFromElements,
-  createBrowserRouter
-} from 'react-router-dom'  
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
+  createBrowserRouter,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-// private routes
-import PrivateRoute from './components/PrivateRoute.jsx'
-
-// Auth
-import Login from './pages/Auth/Login.jsx'
-import Register from './pages/Auth/Register.jsx'
-import Profile from './pages/User/Profile.jsx'
-import ForgotPassword from './pages/Auth/ForgotPassword.jsx'
-import ResetPassword from './pages/Auth/ResetPassword.jsx'
-
-// Admin
-import AdminRoute from './pages/Admin/AdminRoute.jsx'
-import UserList from './pages/Admin/UserList.jsx'
-import CategoryList from './pages/Admin/CategoryList.jsx'
-import ProductList from './pages/Admin/ProductList.jsx'
-import AllProducts from './pages/Admin/AllProducts.jsx'
-import ProductUpdate from './pages/Admin/ProductUpdate.jsx'
-import OrderList from './pages/Admin/OrderList.jsx'
-import AdminDashboard from './pages/Admin/AdminDashboard.jsx'
-
-// User & Shop
-import Home from './pages/Home.jsx'
-import Favorites from './pages/Products/Favorites.jsx'
-import ProductDetails from './pages/Products/ProductDetails.jsx'
-import Cart from './pages/Cart.jsx'
-import Shop from './pages/Shop.jsx'
-import Shipping from './pages/Orders/Shipping.jsx'
-import PlaceOrder from './pages/Orders/PlaceOrder.jsx'
-import Order from './pages/Orders/Order.jsx'
-import UserOrder from './pages/User/UserOrder.jsx'
-
-// Footer pages
-import AboutUs from './pages/Footer/AboutUs.jsx'
-import ContactUs from './pages/Footer/ContactUs.jsx'
-import FAQ from './components/Faq.jsx'
-import TermsOfService from './components/TermsOfService.jsx'
-
+// private routes'
+import PrivateRoute from "./components/PrivateRoute.jsx";
+//Auth
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
+import Profile from "./pages/User/Profile.jsx";
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
+import ProductList from "./pages/Admin/ProductList.jsx";
+import AllProducts from "./pages/Admin/AllProducts.jsx";
+import ProductUpdate from "./pages/Admin/ProductUpdate.jsx";
+import Home from "./pages/Home.jsx";
+import Favorites from "./pages/Products/Favorites.jsx";
+import ProductDetails from "./pages/Products/ProductDetails.jsx";
+import Cart from "./pages/Cart.jsx";
+import Shop from "./pages/Shop.jsx";
+import Shipping from "./pages/Orders/Shipping.jsx";
+import PlaceOrder from "./pages/Orders/PlaceOrder.jsx";
+import Order from "./pages/Orders/Order.jsx";
+import UserOrder from "./pages/User/UserOrder.jsx";
+import OrderList from "./pages/Admin/OrderList.jsx";
+import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
+import AboutUs from "./pages/Footer/AboutUs.jsx";
+import ContactUs from "./pages/Footer/contactUs.jsx";
+import FAQ from "./components/Faq.jsx";
+import TermsOfService from "./components/TermsOfService.jsx";
+import ForgotPassword from "./pages/Auth/ForgotPassword.jsx";
+import ResetPassword from "./pages/Auth/ResetPassword.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      {/* Public routes */}
-      <Route index={true} path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/forgot-password' element={<ForgotPassword />} />  {/* ✅ */}
-      <Route path='/reset-password/:token' element={<ResetPassword />} /> {/* ✅ */}
-
-      <Route path='favorite' element={<Favorites />}/>
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="favorite" element={<Favorites />} />
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/shop" element={<Shop />} />
@@ -69,12 +59,11 @@ const router = createBrowserRouter(
       <Route path="/faq" element={<FAQ />} />
       <Route path="/terms" element={<TermsOfService />} />
 
-      {/* Protected (logged-in) routes */}
-      <Route path='' element={<PrivateRoute />}>
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/shipping' element={<Shipping />} />
-        <Route path='/placeorder' element={<PlaceOrder />} />
-        <Route path='/order/:id' element={<Order />} />
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/order/:id" element={<Order />} />
       </Route>
 
       {/* Admin Routes */}
@@ -90,10 +79,10 @@ const router = createBrowserRouter(
       </Route>
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
-)
+);
